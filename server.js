c51c16cb9c34a827bc860f74e85c397d89fa5d45
santhosh vempali <vepalisantosh@gmail.com>
2022-05-11 15:47:32 +0530

    Revert "added login and signup"
    This reverts commit 63f00baa7d50af0a6eacd899524e76c42fd582dc.

--- server.js
+++ server.js
@@ -6,22 +6,13 @@ const path=require("path")
 const PORT=process.env.PORT || 4000
 app=express()
 app.use(express.static("public"))
-
-app.use(expressLayouts)
-app.set('views',path.join(__dirname,'/resources/views'))
-app.set('view engine','ejs')
 app.get("/",(req,res)=>{
     res.render("home");
 })
-app.get("/cart",(req,res)=>{
-    res.render("customers/cart")
-})
-app.get("/login",(req,res)=>{
-    res.render("auth/login")
-})
-app.get("/register",(req,res)=>{
-    res.render("auth/register")
-})
+app.use(expressLayouts)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
+
 
 
 try {
